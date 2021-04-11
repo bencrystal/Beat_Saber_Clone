7361ae763aa4588675601e28761ba0ec41194424
bencrystal <46793135+bencrystal@users.noreply.github.com>
2021-04-11 14:49:31 -0400

    Block Spawner Added
    Blocks Spawn at a constant rate, not officially tied to a song yet though!

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -6,26 +6,27 @@ public class Spawner : MonoBehaviour
 {
 
     public GameObject[] cubes;
-    public Transform[] points;
-    public float beat = (60/105)*2;
-    private float _timer;
+    public Transform[] points; //points where cubes are instantiated
+    public float tempo = (60/105)*2;
+    
+    private float _timer; //time between 2 beats or spawns of the cube
 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_timer > beat)
+        if (_timer > tempo)
         {
-            GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[Random.Range(0, 4)]);
+            GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[Random.Range(0, 9)]);
             cube.transform.localPosition = Vector3.zero;
             cube.transform.Rotate(transform.forward, 45 * Random.Range(0,8)); //random 1 of 8 cardinal directions
-            _timer -= beat;
+            _timer -= tempo;
         }
 
         _timer += Time.deltaTime;
